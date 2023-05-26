3c7d32d9d5e4cf33f23f132f570791a9f7e67c6c
Sahad <sahadmuhammed289@gmail.com>
2023-05-26 18:24:32 +0530

    project compolteted

--- src/components/homepage/homepage.jsx
+++ src/components/homepage/homepage.jsx
@@ -11,7 +11,6 @@ function HomePage() {
     const state = useSelector(state => { return state });
     const { spinner } = state;
     var navigate = useNavigate();
-
     useEffect(() => {
         let token = localStorage.getItem('user_auth_app_token');
         if (token) getUserDetailsAndVerifyToken(token).then(response => {
@@ -46,21 +45,21 @@ function HomePage() {
             <div className='mybody'>
                 <div className="container position-relative">
                     <div className="row ">
-                        <div className="col-12  profilebody text-white text-end py-3"><button className=' text-white btnforlgout px-3 rounded py-1 text-uppercase' onClick={logout}>Logout ></button></div>
+                        <div className="col-12  profilebody text-white text-end py-3"><button className='bg-inherit text-white border-dark btnforlgout px-3 rounded py-1 text-uppercase' onClick={logout}>Logout</button></div>
                     </div>
                     <div className="profileRound ">
-                        <div className='text-white font-bold'><h1>P</h1></div>
+                        {
+                            user.profile ? <img src={user.profile} alt="..." className='profileImage' /> : <img src="https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o=" alt="" className='profileImage' />
+                        }
                     </div>
                 </div>
             </div>
             {handleSpinner()}
             <div className="container">
                 <div className="row d-flex justify-content-center align-items-center my-5 py-5">
-                    <div className="col-5 profileInfo py-5 text-center rounded ">
-                        <h4>{user.username}</h4>
-                        <h4>{user.email}</h4>
-                        <h5>UID : {user._id}</h5>
-                    </div>
+                    <h4>{user.username}</h4>
+                    <h4>{user.email}</h4>
+                    <h5>UID : {user._id}</h5>
                 </div>
             </div>
         </div>
